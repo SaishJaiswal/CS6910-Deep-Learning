813e3282e28eca5690ddbf3842781e815cbc7089
Saish Jaiswal <cs20d405@smail.iitm.ac.in>
2021-03-15 20:18:24 +0530

    dl-assignment-1-with-wandb

--- Assignment_1/run.sh
+++ Assignment_1/run.sh
@@ -4,7 +4,7 @@
 #python -m wandb sweep --project "project_name" sweep.yaml
 #python -m wandb agent "saish/project_name/sweep_id"
 
-#python -m wandb sweep --project "demo" sweep.yaml
-#python -m wandb agent "saish/demo/gn0pd3id"
+#python -m wandb sweep --project "Deep-Learning" sweep.yaml
+#python -m wandb agent "saish/Deep-Learning/1wyfksde"
 
-python main.py -l 784 128 32 10 --optimizer nadam --epochs 5 --l_rate 0.001 --batch_size 64 --loss cross_entropy --activation relu --n_hlayers 2 --hlayer_size 32
+python main.py --optimizer sgd --epochs 5 --l_rate 0.001 --batch_size 32 --loss cross_entropy --activation tanh --n_hlayers 3 --hlayer_size 128
